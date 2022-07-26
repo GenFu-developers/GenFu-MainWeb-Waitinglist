aeccb5963bd4ee580e1a0218b5e4cf674e065d23
realpablit0 <83354756+realpablit0@users.noreply.github.com>
2022-07-26 12:06:43 +0200

    fixed much things and smooth!

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,18 @@
 (() => {
+    console.log('inint');
     const moevLogo = () => {
         const e = document.getElementById('circleLogo')
+        const _e = document.getElementById('circleLogoText')
         const top = window.pageYOffset;
         if (top > 20) {
             e.style.translate = '200px'
+            _e.style.translate = '200px'
         }
         else if (top < 20) {
             e.style.translate = '0px'
+            _e.style.translate = '0px'
         }
+        return
     }
 
     const moveScrollArrow = () => {
@@ -19,11 +24,23 @@
         else if (top < 20) {
             e.style.translate = '0px'
         }
+        return
     }
 
-    document.addEventListener('scroll', (e) => {
+    var lastScrollTop = 0;
+    document.addEventListener('scroll', () => {
         moevLogo()
         moveScrollArrow()
+        const documentE = document.documentElement
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop) {
+            // downscroll code
+            documentE.scrollTop = documentE.scrollHeight - documentE.clientHeight;
+        } else {
+            // upscroll code
+            documentE.scrollTop = 0
+        }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
     })
 })()
 
